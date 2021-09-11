262b60f2e8514ecb13cac215ae67b680a924ee51
Matyas Angyal <amatyas001@gmail.com>
2021-09-11 16:12:58 +0200

    rebase
    rebase
    
    rebase
    
    rebase
    
    rebase

---
+++ src/strapi/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8bcab3e57d416071e861d036173291c'),
+    },
+  },
+});
---
+++ src/strapi/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb350b17-fd3a-4880-9f2d-05da6e50b31c'
+};
